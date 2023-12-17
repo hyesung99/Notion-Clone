b6526ad191738dbdadb0cf3d9847a6072197c4c6
hyesung99 <4538asd@gmail.com>
2023-12-17 16:15:46 +0900

    feat : API엔드포인트 상수화

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,7 @@
 export const DOCUMENT_KEY = '/documents'
 
 export const NOTION_API_END_POINT = 'https://kdt-frontend.programmers.co.kr'
+export const END_POINTS = {
+  DOCUMENT_TREE: '/documents',
+  DOCUMENT_DETAIL: (documentId) => `/documents/${documentId}`,
+}
