9a511ba977c7af92d45557f1c78d17d80ac11f25
hyesung99 <4538asd@gmail.com>
2023-12-18 15:19:15 +0900

    refactor : documentTree api 재정의

--- src/apis/documentTree.api.js
+++ src/apis/documentTree.api.js
@@ -5,3 +5,17 @@ export const getDocumentTree = async () => {
   const data = await NotionFetchClient.get(END_POINTS.DOCUMENT_TREE)
   return data
 }
+
+export const postDocumentBranch = async ({ title, parentId }) => {
+  console.log(title, parentId, END_POINTS.DOCUMENT_TREE)
+  const data = await NotionFetchClient.post(END_POINTS.DOCUMENT_TREE, {
+    title,
+    parentId,
+  })
+  return data
+}
+
+export const deleteDocumentBranch = async ({ id }) => {
+  const data = await NotionFetchClient.delete(END_POINTS.DOCUMENT_DETAIL(id))
+  return data
+}
