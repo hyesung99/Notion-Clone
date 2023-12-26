00876268fb807ce225cfbf81d78f9d4ba5c1f930
hyesung99 <4538asd@gmail.com>
2023-12-26 23:50:25 +0900

    feat : reducer 액션 정의

--- src/core/reducer.js
+++ src/core/reducer.js
@@ -1,9 +1,31 @@
-export function rootReducer(state = {}, action) {
+export const rootReducer = (state = {}, action = {}) => {
   return {
-    documentTreeReducer: documentTreeReducer(state.documentTreeReducer, action),
-    documentEditorReducer: documentEditorReducer(
-      state.documentEditorReducer,
-      action
-    ),
+    documentTree: documentTreeReducer(state.documentTree, action),
+    documentEditor: documentEditorReducer(state.documentEditor, action),
   }
 }
+
+export const documentTreeReducer = (state = { documents: [] }, action) => {
+  switch (action.type) {
+    case 'ADD_DOCUMENT':
+      return {
+        ...state,
+        documents: [...state.documentTree, action.payload],
+      }
+    case 'DELETE_DOCUMENT':
+      return {
+        ...state,
+        documents: state.documentTree.filter(
+          (document) => document.id !== action.payload.id
+        ),
+      }
+    case 'SET_DOCUMENT_TREE':
+      return {
+        ...state,
+        documents: action.payload,
+      }
+    default:
+      return state
+  }
+}
+export const documentEditorReducer = () => {}
