1f711ef3b3f934793111e683e18c99e2c33526d7
hyesung99 <4538asd@gmail.com>
2024-01-03 16:08:43 +0900

    feat : 에디터 side를 위한 api 정의

--- src/apis/document.api.js
+++ src/apis/document.api.js
@@ -6,7 +6,12 @@ export const getDocumentTree = async () => {
   return data
 }
 
-export const postDocumentBranch = async ({ title, parentId }) => {
+export const getDocumentDetail = async ({ id }) => {
+  const data = await NotionFetchClient.get(END_POINTS.DOCUMENT_DETAIL(id))
+  return data
+}
+
+export const postDocument = async ({ title, parentId }) => {
   const data = await NotionFetchClient.post(END_POINTS.DOCUMENT_TREE, {
     title,
     parent: parentId,
@@ -14,7 +19,15 @@ export const postDocumentBranch = async ({ title, parentId }) => {
   return data
 }
 
-export const deleteDocumentBranch = async ({ id }) => {
+export const deleteDocument = async ({ id }) => {
   const data = await NotionFetchClient.delete(END_POINTS.DOCUMENT_DETAIL(id))
+  return data
+}
+
+export const putDocument = async ({ id, title, body }) => {
+  const data = await NotionFetchClient.put(END_POINTS.DOCUMENT_DETAIL(id), {
+    title,
+    body,
+  })
   return data
 }
