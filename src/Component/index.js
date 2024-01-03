972ae6851b3c18a36d337dd5411d87ce108b5d97
hyesung99 <4538asd@gmail.com>
2024-01-03 15:03:03 +0900

    remove : index alias 방식 제거

--- src/Component/index.js
+++
@@ -1,3 +0,0 @@
-export { default as DocumentTreeBranchComponent } from './DocumentBranch.component.js'
-export { default as DocumentTreeComponent } from './DocumentTree.component.js'
-export { default as EditorComponent } from './Editor.component.js'
