c46922a0663bf1605e752762c103586b8212a2ec
hyesung99 <4538asd@gmail.com>
2024-01-06 23:22:40 +0900

    refactor : detail page 구조 변경

--- src/page/detail.page.js
+++ src/page/detail.page.js
@@ -7,32 +7,46 @@ import {
   setDocumentDetailThunk,
 } from '../store/reducer.js'
 import { selectDocumentDetail } from '../store/selector.js'
+import DetailTitleComponent from '../component/DetailTitle.component.js'
 
 export default class DetailPage extends Component {
   template() {
-    const { title, content } = useSelector(selectDocumentDetail)
+    // const { title, content } = useSelector(selectDocumentDetail)
     return `
       <div class="detail-page">
-        <input class="detail-title" value=${title}/>
-        <div class="detail-content">${content || '내용을 입력해주세요'}</div>
+        <section class="detail-header"/>
+        <section class="detail-body"/>
       </div>
     `
   }
 
   created() {
-    store.subscribe(this.render.bind(this))
     const id = getDetailId()
     store.dispatch(setDocumentDetailThunk({ id }))
   }
 
-  mounted() {
-    this.setEvent('change', '.detail-title', (event) => {
-      store.dispatch(
-        putDocumentTitleThunk({
-          title: event.target.value,
-          id: getDetailId(),
-        })
-      )
+  render() {
+    this.$target.innerHTML = this.template()
+    const id = getDetailId()
+    const { title, content } = useSelector(selectDocumentDetail)
+
+    this.createChildComponent({
+      component: DetailTitleComponent,
+      componentOptions: {
+        $target: document.querySelector('.detail-header'),
+        props: { title, id },
+      },
     })
   }
+
+  // mounted() {
+  //   this.setEvent('change', '.detail-title', (event) => {
+  //     store.dispatch(
+  //       putDocumentTitleThunk({
+  //         title: event.target.value,
+  //         id: getDetailId(),
+  //       })
+  //     )
+  //   })
+  // }
 }
