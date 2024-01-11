1da9d2ac8734dbefd15190c05e1fdd7fa365edf5
hyesung99 <4538asd@gmail.com>
2024-01-11 17:10:13 +0900

    remove : 사용하지 않는 파일들 삭제

--- src/domain/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Document } from "./Document.js";
-export { default as DocumentTree } from "./DocumentTree.js";
-export { default as DocumentTreeBranch } from "./DocumentTreeBranch.js";
-export { initDocument, initComponent } from "./initData.js";
--- src/service/index.js
+++
@@ -1,6 +0,0 @@
-export { getDocument } from './documentService.js'
-export { saveDocumentToServer } from './documentService.js'
-export { saveDocumentToStorage } from './documentStorageService.js'
-export { removeDocumentFromStorage } from './documentStorageService.js'
-export { getDocumentFromStorage } from './documentStorageService.js'
-export { cloneDomain } from './domainService.js'
