fd30f727801ef20b68d5a42f0a96706b5d769acd
hyesung99 <4538asd@gmail.com>
2024-01-24 22:01:45 +0900

    update : webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import App from "./App.js";
-const $app = document.querySelector("#app");
+import App from './App.js'
+import './style.css'
 
-new App({  $target: $app });
+const $app = document.querySelector('#app')
+
+new App({ $target: $app })
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-const dirname = path.dirname(__dirname)
-
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
@@ -24,13 +22,13 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './index.html' }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({ filename: 'app.css' }),
   ],
 }
