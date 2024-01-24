827ac1ba1921286e8930c9824a248253c61eaf04
hyesung99 <4538asd@gmail.com>
2024-01-24 16:06:39 +0900

    fix : homePage.js => HomePage.js

--- src/router/hashRouter.js
+++ src/router/hashRouter.js
@@ -1,5 +1,5 @@
 import { createHashRouter } from '../core/createHashRouter.js'
-import HomePage from '../page/homePage.js'
+import HomePage from '../page/HomePage.js'
 import DetailPage from '../page/DetailPage.js'
 
 const routes = {
