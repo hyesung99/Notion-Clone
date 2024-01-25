c67cf24cff464dab2a0b4222b4a6544377e6335a
hyesung99 <4538asd@gmail.com>
2024-01-25 14:38:28 +0900

    feat : branch title 변경 focustout 이벤트에 할당

--- src/component/DetailTitle.js
+++ src/component/DetailTitle.js
@@ -1,7 +1,7 @@
 import Component from '../core/Component.js'
 import { store } from '../core/createStore.js'
 import useSelector from '../service/useSelector.js'
-import { putBranchTitle, putDocumentTitleThunk } from '../store/reducer.js'
+import { setBranchTitle, putDocumentTitleThunk } from '../store/reducer.js'
 import { selectDetailTitle } from '../store/selector.js'
 import { getDetailId } from '../service/getDetailId.js'
 
@@ -19,21 +19,18 @@ export default class DetailTitle extends Component {
     store.subscribe(this.render.bind(this))
   }
 
+  render() {
+    this.$target.innerHTML = this.template()
+  }
+
   mounted() {
-    this.setEvent('input', '.detail-title', (event) => {
+    this.setEvent('focusout', '.detail-title', (event) => {
       store.dispatch(
-        putBranchTitle({
+        setBranchTitle({
           title: event.target.value,
           id: getDetailId(),
         })
       )
-    })
-
-    this.setEvent('focusin', '.detail-title', () =>
-      store.unsubscribe(this.render.bind(this))
-    )
-    this.setEvent('focusout', '.detail-title', (event) => {
-      store.subscribe(this.render.bind(this))
       store.dispatch(
         putDocumentTitleThunk({
           title: event.target.value,
